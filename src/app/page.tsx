d22917e23650583d02caa135bd2b429e18697480
johnhm22 <johnhowardmorgan@outlook.com>
2025-02-19 11:01:54 +0000

    Adding mobile responsive styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
-import Image from 'next/image';
-import { useState } from 'react';
+import { useChat } from "ai/react";
+import Image from "next/image";
+import { useState } from "react";
 
-import france from './assets/france.png';
-import spain from './assets/spain.png';
-import japan from './assets/japan.png';
+import france from "./assets/france.png";
+import spain from "./assets/spain.png";
+import japan from "./assets/japan.png";
 
 export default function Home() {
-  type Language = 'French' | 'Spanish' | 'Japanese';
+  type Language = "French" | "Spanish" | "Japanese";
   const [language, setLanguage] = useState<Language | null>();
 
   const { messages, input, handleInputChange, handleSubmit } = useChat({
@@ -19,91 +19,91 @@ export default function Home() {
   });
 
   return (
-    <section className='min-h-screen flex justify-center items-center bg-slate-300'>
-      <main className='bg-zinc-100 rounded-lg w-1/3'>
-        <div className='border border-gray-300 m-3 rounded-lg'>
-          <div className=' font-[family-name:var(--font-geist-sans)]'>
-            <header className='bg-[url(./assets/continents.png)] bg-contain bg-center mb-3 rounded-t-lg h-32 content-center flex justify-center'></header>
-            <section className='flex flex-col h-1/2 gap-3 mb-7 mx-3 border-2 border-black rounded-md'>
-              <div className='flex flex-col gap-1'>
-                <div className='text-blue-700 font-semibold text-md mb-3 mx-1 text-center'>
+    <section className="min-h-screen flex justify-center items-center bg-slate-300">
+      <main className="bg-zinc-100 rounded-lg w-2/3 md:w-1/2 lg:w-1/3">
+        <div className="border border-gray-300 m-3 rounded-lg">
+          <div className=" font-[family-name:var(--font-geist-sans)]">
+            <header className="bg-[url(./assets/continents.png)] bg-contain bg-center mb-3 rounded-t-lg h-32 content-center flex justify-center"></header>
+            <section className="flex flex-col h-1/2 gap-3 mb-7 mx-3 border-2 border-black rounded-md">
+              <div className="flex flex-col gap-1">
+                <div className="text-blue-700 font-semibold text-sm md:text-base mb-3 mx-1 text-center">
                   Select the language for the response by clicking the flag at
                   the bottom
                 </div>
-                <div className='overflow-y-auto h-auto max-h-52'>
+                <div className="overflow-y-auto h-auto max-h-52">
                   {messages.map((m) => (
                     <div
                       className={
-                        m.role === 'user'
-                          ? 'bg-cyan-200 rounded-md mx-5 p-2 mb-2'
-                          : 'bg-gray-200 rounded-md mx-5 p-2 mb-2'
+                        m.role === "user"
+                          ? "bg-cyan-200 rounded-md mx-5 p-2 mb-2"
+                          : "bg-gray-200 rounded-md mx-5 p-2 mb-2"
                       }
                       key={m.id}
                     >
-                      {m.role === 'user' ? 'User: ' : 'AI: '}
+                      {m.role === "user" ? "User: " : "AI: "}
                       {m.content}
                     </div>
                   ))}
                 </div>
               </div>
               <form onSubmit={handleSubmit}>
-                <div className=' flex flex-col justify-center gap-3'>
+                <div className=" flex flex-col justify-center gap-3">
                   <input
                     suppressHydrationWarning
-                    placeholder='type question here'
-                    className='bg-slate-200 rounded-md mx-5 p-2'
-                    name='text'
+                    placeholder="type question here"
+                    className="bg-slate-200 rounded-md mx-5 p-2 text-sm lg:text-lg"
+                    name="text"
                     value={input}
                     onChange={handleInputChange}
                   />
                 </div>
-                <span className=' mt-4 mb-2 flex justify-center'>
+                <span className=" mt-4 mb-2 flex justify-center">
                   <button
                     suppressHydrationWarning
-                    type='submit'
-                    className='bg-blue-800 text-xl text-white font-semibold w-5/6 py-1 rounded-md'
+                    type="submit"
+                    className="bg-blue-800 text-sm lg:text-xl text-white font-semibold w-5/6 py-1 rounded-md"
                   >
                     Send
                   </button>
                 </span>
-                <span className='px-3 mb-3 flex flex-row gap-2 justify-between'>
+                <span className="px-3 mb-3 flex flex-row gap-2 justify-between">
                   <Image
                     src={france}
                     height={35}
                     width={35}
-                    alt='French flag'
+                    alt="French flag"
                     className={
-                      language === 'French'
-                        ? 'hover:cursor-pointer shadow-xl'
-                        : 'hover:cursor-pointer'
+                      language === "French"
+                        ? "hover:cursor-pointer shadow-xl"
+                        : "hover:cursor-pointer"
                     }
-                    data-lang='french'
-                    onClick={() => setLanguage('French')}
+                    data-lang="french"
+                    onClick={() => setLanguage("French")}
                   />
                   <Image
                     src={spain}
                     height={35}
                     width={35}
-                    alt='Spanish flag'
+                    alt="Spanish flag"
                     className={
-                      language === 'Spanish'
-                        ? 'hover:cursor-pointer shadow-xl'
-                        : 'hover:cursor-pointer'
+                      language === "Spanish"
+                        ? "hover:cursor-pointer shadow-xl"
+                        : "hover:cursor-pointer"
                     }
-                    onClick={() => setLanguage('Spanish')}
+                    onClick={() => setLanguage("Spanish")}
                   />
                   <Image
                     src={japan}
                     height={35}
                     width={35}
-                    alt='Japanese flag'
+                    alt="Japanese flag"
                     className={
-                      language === 'Japanese'
-                        ? 'hover:cursor-pointer shadow-xl'
-                        : 'hover:cursor-pointer'
+                      language === "Japanese"
+                        ? "hover:cursor-pointer shadow-xl"
+                        : "hover:cursor-pointer"
                     }
-                    data-lang='japanese'
-                    onClick={() => setLanguage('Japanese')}
+                    data-lang="japanese"
+                    onClick={() => setLanguage("Japanese")}
                   />
                 </span>
               </form>
